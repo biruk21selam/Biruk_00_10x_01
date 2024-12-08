6c2c851d31d70b9ea61ca975e39d01e0f82b3e17
biruk21selam <biruk16selam@gmail.com>
2024-12-08 22:24:52 +0300

    tes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,7 @@
-ECHO is on.
+import pytest
+from app.utils import load_data
+
+def test_load_data():
+   data = load_data()
+   assert data is not None  # Check that data is loaded.
+   assert 'GHI' in data.columns  # Check that 'GHI' column exists.
