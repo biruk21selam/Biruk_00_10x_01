adcdc34c7038fa29eb7bbe3ed4dae43a22caf2bf
biruk21selam <biruk16selam@gmail.com>
2024-12-08 20:58:30 +0300

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,1 +1,12 @@
+from src.data_loader import load_data
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    file_paths = ["data/file1.xlsx", "data/file2.xlsx"]
+    data = load_data(file_paths)
+
+    # Plot example
+    data['GHI'].hist()
+    plt.title("GHI Distribution")
+    plt.show()
 
